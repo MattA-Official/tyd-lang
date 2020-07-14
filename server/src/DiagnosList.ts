1d287513b3f1bb67b456fad77e567b549c057103
Calum Henderson <calumproh28@gmail.com>
2020-07-14 10:58:11 +0100

    Added some errors.

--- server/src/DiagnosList.ts
+++ server/src/DiagnosList.ts
@@ -19,7 +19,7 @@ const nsq = {
         this.diagnostic.range.end = e.positionAt(m.index + m[0].length)
     }
 }
-const ncs = {
+const nsc = {
     pattern:  /".*?((?!\\;);).*?"/g,
     code: "TyD-ncs",
     diagnostic: {
@@ -35,6 +35,77 @@ const ncs = {
         this.diagnostic.range.end = e.positionAt(m.index + m[0].length)
     }
 }
-const list: Diagno[] = [nsq, ncs];
+const nsb = {
+    pattern:  /"(.*?((?!\\\[)\[).*?)|(.*?((?!\\\])\]).*?)"/g,
+    code: "TyD-nsb",
+    diagnostic: {
+        severity: DiagnosticSeverity.Error,
+        range: {
+            start: Position.create(0,0),
+            end: Position.create(0,0)
+        },
+        message: "Square bracket(s) not escaped in string."
+    },
+    setRange(e: TextDocument, m: RegExpExecArray) {
+        this.diagnostic.range.start = e.positionAt(m.index),
+        this.diagnostic.range.end = e.positionAt(m.index + m[0].length)
+    }
+}
+const ncb = {
+    pattern:  /"(.*?((?!\\\{){).*?)|(.*?((?!\\\})}).*?)"/g,
+    code: "TyD-ncb",
+    diagnostic: {
+        severity: DiagnosticSeverity.Error,
+        range: {
+            start: Position.create(0,0),
+            end: Position.create(0,0)
+        },
+        message: "Curly bracket(s) not escaped in string."
+    },
+    setRange(e: TextDocument, m: RegExpExecArray) {
+        this.diagnostic.range.start = e.positionAt(m.index),
+        this.diagnostic.range.end = e.positionAt(m.index + m[0].length)
+    }
+}
+const idc = {
+    pattern:  /".*?((?!\\\#)#).*?"/g,
+    code: "TyD-idc",
+    diagnostic: {
+        severity: DiagnosticSeverity.Warning,
+        range: {
+            start: Position.create(0,0),
+            end: Position.create(0,0)
+        },
+        message: "Incidental Comment?"
+    },
+    setRange(e: TextDocument, m: RegExpExecArray) {
+        this.diagnostic.range.start = e.positionAt(m.index),
+        this.diagnostic.range.end = e.positionAt(m.index + m[0].length)
+    }
+}
+const ibs = {
+    pattern:  /".*?((?!\\\\)\\[\S]).*?"/g,
+    code: "TyD-idc",
+    diagnostic: {
+        severity: DiagnosticSeverity.Error,
+        range: {
+            start: Position.create(0,0),
+            end: Position.create(0,0)
+        },
+        message: "Invalid escape character. If you wanted the \"\\\" character by itself, use \"\\\\\" instead.",
+    },
+    setRange(e: TextDocument, m: RegExpExecArray) {
+        this.diagnostic.range.start = e.positionAt(m.index),
+        this.diagnostic.range.end = e.positionAt(m.index + m[0].length)
+    }
+}
+const list: Diagno[] = [
+    nsq, 
+    nsc,
+    nsb,
+    ncb,
+    ibs,
+    idc
+];
 
 module.exports = list;
